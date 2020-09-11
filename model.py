4d1b493f2f0bc6cc5d6b7fd99c10c0709e717277
wwt <wwt1346138@163.com>
2020-09-11 18:34:24 +0800

    change for models

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import os
 import math
 import sys
-
+import random
 import torch
 import torch.nn as nn
 import numpy as np
@@ -9,9 +9,9 @@ import torch.nn.functional as Func
 from torch.nn import init
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
-
+from constants import *
 import torch.optim as optim
-
+from utils import seq_to_graph
 
 
 
@@ -155,47 +155,155 @@ class st_gcn(nn.Module):
 
         return x, A
 
-class social_stgcnn(nn.Module):
-    def __init__(self,n_stgcnn =1,n_txpcnn=1,input_feat=2,output_feat=5,
-                 seq_len=8,pred_seq_len=12,kernel_size=3):
-        super(social_stgcnn,self).__init__()
-        self.n_stgcnn= n_stgcnn     # stgcn 的数量
-        self.n_txpcnn = n_txpcnn    # tcn的数量
-                
-        self.st_gcns = nn.ModuleList()
-        self.st_gcns.append(st_gcn(input_feat,output_feat,(kernel_size,seq_len)))
-        for j in range(1,self.n_stgcnn):        # 产生GCN序列
-            self.st_gcns.append(st_gcn(output_feat,output_feat,(kernel_size,seq_len)))
-        
-        self.tpcnns = nn.ModuleList()
-        self.tpcnns.append(nn.Conv2d(seq_len,pred_seq_len,3,padding=1))
-        for j in range(1,self.n_txpcnn):        # 产生TCN序列
-            self.tpcnns.append(nn.Conv2d(pred_seq_len,pred_seq_len,3,padding=1))
-        self.tpcnn_ouput = nn.Conv2d(pred_seq_len,pred_seq_len,3,padding=1)
-            
-            
-        self.prelus = nn.ModuleList()
-        for j in range(self.n_txpcnn):
-            self.prelus.append(nn.PReLU())
+def make_mlp(dim_list):
+    '''
+    批量生成全连接层
+    :param dim_list: 维度列表
+    :return: 一系列线性层的感知机
+    '''
+    layers = []
+    for dim_in, dim_out in zip(dim_list[:-1], dim_list[1:]):
+        layers.append(nn.Linear(dim_in, dim_out))
+        layers.append(nn.ReLU())
+    return nn.Sequential(*layers)
 
+def get_noise(shape):
+    '''
+    生成高斯噪声
+    :param shape: 噪声的shape
+    :return:
+    '''
+    return torch.randn(*shape).cuda()
 
-        
-    def forward(self,v,a):
+class Encoder(nn.Module):
+    def __init__(self,out_channels=5):
+        super(Encoder, self).__init__()
 
-        for k in range(self.n_stgcnn):
-            v,a = self.st_gcns[k](v,a)
-            
-        v = v.view(v.shape[0],v.shape[2],v.shape[1],v.shape[3])
-        
-        v = self.prelus[0](self.tpcnns[0](v))
+        self.h_dim = H_DIM  # 64 
+        self.embedding_dim = out_channels   # 节点通道
 
-        for k in range(1,self.n_txpcnn-1):
-            v =  self.prelus[k](self.tpcnns[k](v)) + v      # 类似残差结构
-            
-        v = self.tpcnn_ouput(v)
-        v = v.view(v.shape[0],v.shape[2],v.shape[1],v.shape[3])
+        self.encoder = nn.LSTM(self.embedding_dim, self.h_dim, 1)        # 输入的维度=out_chanels，隐状态维度64，层数1，LSTM编码
+        # self.spatial_embedding = nn.Linear(2, self.embedding_dim)           # 输入2维，输出embedding_dim
+
+    def init_hidden(self, batch):
+        '''
+        隐层状态初始化，细胞状态初始化
+        :param batch:
+        :return:
+        '''
+        h = torch.zeros(1, batch, self.h_dim).cuda()        # 1*bat*64
+        c = torch.zeros(1, batch, self.h_dim).cuda()
+        return (h, c)
+
+    def forward(self, V):       # v={N C T V}
+
+        # padded = len(obs_trajV.shape) == 4    # 观测轨迹的维度数目4
+        npeds = V.size(3)             # 节点数
+        total = npeds * V.size(1)        # Bantch=npeds*bantch
+
+        # obs_traj_embedding = self.spatial_embedding(obs_traj.view(-1, 2))   # 展成n*2输入线性嵌入层,输出16 8*3*64*16
+        V = V.view(-1, total, self.embedding_dim)  # 调整维度（-1，4096，16）
+        state = self.init_hidden(total)        # 以total初始化隐藏状态 1*4096*64
+        output, state = self.encoder(V, state)         # 输入编码器输出到state中64
+        final_h = state[0]      # 取隐藏状态
         
+        final_h = final_h.view(npeds, self.h_dim)
+        return final_h      # 最后一步的输出
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        self.seq_len = PRED_LEN         # 解码长度 12
+        self.h_dim = H_DIM              # 64
+        self.embedding_dim = EMBEDDING_DIM      # 16
+
+        self.decoder = nn.LSTM(self.embedding_dim, self.h_dim, 1)   # 输入为16 ，输出为64 ，1层隐藏层
+        self.spatial_embedding = nn.Linear(2, self.embedding_dim)   # 输入2输出16
+        self.hidden2pos = nn.Linear(self.h_dim, 2)                  # 转换为坐标序列 输入64，输出2
+
+    def forward(self, last_pos, last_pos_rel, state_tuple):
+        npeds = last_pos.size(0)        # 行人编号
+        pred_traj_fake_rel = []
+        decoder_input = self.spatial_embedding(last_pos_rel)        # 输入64*2，输出维度64*16
+        decoder_input = decoder_input.view(1, npeds, self.embedding_dim)        # 调整维度1*64*16
+
+        for _ in range(self.seq_len):
+            output, state_tuple = self.decoder(decoder_input, state_tuple)      # 输出1*64*64的隐状态和细胞状态
+            rel_pos = self.hidden2pos(output.view(-1, self.h_dim))              # 将out变为64*64再输入坐标转换64*2
+            curr_pos = rel_pos + last_pos   # 当前坐标为坐标差和最后坐标之和
+            embedding_input = rel_pos       # 64*2
+
+            decoder_input = self.spatial_embedding(embedding_input)     # 输入64*2输出64*16
+            decoder_input = decoder_input.view(1, npeds, self.embedding_dim)   # 变为 1*64*16
+            pred_traj_fake_rel.append(rel_pos.view(npeds, -1))   # 预测轨迹加入rel_pos  12*64*2
+            last_pos = curr_pos  # 当前坐标作为最后坐标
+
+        pred_traj_fake_rel = torch.stack(pred_traj_fake_rel, dim=0)     # 按0维拼接
+        return pred_traj_fake_rel       # 12*64*2
+class endPointGenerate(nn.Module):
+    def __init__(self,out_channels=5):
+        super(Encoder, self).__init__()
+
+        self.h_dim = H_DIM  # 64 
+        self.embedding_dim = out_channels   # 节点通道
+
+        self.encoder = nn.LSTM(self.embedding_dim, self.h_dim, 1)        # 输入的维度=out_chanels，隐状态维度64，层数1，LSTM编码
+        # self.spatial_embedding = nn.Linear(2, self.embedding_dim)           # 输入2维，输出embedding_dim
+
+    def init_hidden(self, batch):
+        '''
+        隐层状态初始化，细胞状态初始化
+        :param batch:
+        :return:
+        '''
+        h = torch.zeros(1, batch, self.h_dim).cuda()        # 1*bat*64
+        c = torch.zeros(1, batch, self.h_dim).cuda()
+        return (h, c)
+
+    def forward(self, V):       # v={N C T V}
+
+        # padded = len(obs_trajV.shape) == 4    # 观测轨迹的维度数目4
+        npeds = V.size(3)             # 节点数
+        total = npeds * V.size(1)        # Bantch=npeds*bantch
+
+        # obs_traj_embedding = self.spatial_embedding(obs_traj.view(-1, 2))   # 展成n*2输入线性嵌入层,输出16 8*3*64*16
+        V = V.view(-1, total, self.embedding_dim)  # 调整维度（-1，4096，16）
+        state = self.init_hidden(total)        # 以total初始化隐藏状态 1*4096*64
+        output, state = self.encoder(V, state)         # 输入编码器输出到state中64
+        final_h = state[0]      # 取隐藏状态
         
-        return v,a
-a=social_stgcnn()
-print(a)
+        final_h = final_h.view(npeds, self.h_dim)
+        return final_h      # 最后一步的输出
+
+class endDecoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        self.seq_len = PRED_LEN         # 解码长度 12
+        self.h_dim = H_DIM              # 64
+        self.embedding_dim = EMBEDDING_DIM      # 16
+
+        self.decoder = nn.LSTM(self.embedding_dim, self.h_dim, 1)   # 输入为16 ，输出为64 ，1层隐藏层
+        self.spatial_embedding = nn.Linear(2, self.embedding_dim)   # 输入2输出16
+        self.hidden2pos = nn.Linear(self.h_dim, 2)                  # 转换为坐标序列 输入64，输出2
+
+    def forward(self, last_pos, last_pos_rel, state_tuple):
+        npeds = last_pos.size(0)        # 行人编号
+        pred_traj_fake_rel = []
+        decoder_input = self.spatial_embedding(last_pos_rel)        # 输入64*2，输出维度64*16
+        decoder_input = decoder_input.view(1, npeds, self.embedding_dim)        # 调整维度1*64*16
+
+        for _ in range(self.seq_len):
+            output, state_tuple = self.decoder(decoder_input, state_tuple)      # 输出1*64*64的隐状态和细胞状态
+            rel_pos = self.hidden2pos(output.view(-1, self.h_dim))              # 将out变为64*64再输入坐标转换64*2
+            curr_pos = rel_pos + last_pos   # 当前坐标为坐标差和最后坐标之和
+            embedding_input = rel_pos       # 64*2
+
+            decoder_input = self.spatial_embedding(embedding_input)     # 输入64*2输出64*16
+            decoder_input = decoder_input.view(1, npeds, self.embedding_dim)   # 变为 1*64*16
+            pred_traj_fake_rel.append(rel_pos.view(npeds, -1))   # 预测轨迹加入rel_pos  12*64*2
+            last_pos = curr_pos  # 当前坐标作为最后坐标
+
+        pred_traj_fake_rel = torch.stack(pred_traj_fake_rel, dim=0)     # 按0维拼接
+        return pred_traj_fake_rel       # 12*64*2
